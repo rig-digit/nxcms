8a415042b946ad5bf1c36b87a716ccd4e96d0336
rig-digit <33613284+rig-digit@users.noreply.github.com>
2020-03-11 21:27:43 +0400

    Add files via upload

---
+++ build/precache-manifest.11d133340a0d592acb25f63747c8d3b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6d2bcebd9adeb27b7444",
+    "url": "/static/js/main.6d2bcebd.chunk.js"
+  },
+  {
+    "revision": "10ed297c80b3ebf7576b",
+    "url": "/static/js/1.10ed297c.chunk.js"
+  },
+  {
+    "revision": "10ed297c80b3ebf7576b",
+    "url": "/static/css/1.7e804dc8.chunk.css"
+  },
+  {
+    "revision": "9fdb58544f739f364b8924198590ef21",
+    "url": "/index.html"
+  }
+];
